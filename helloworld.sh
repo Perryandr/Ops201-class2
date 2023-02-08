ea63b7f9bd16923590d9dd9f9b9f0eb1b137c1bd
Andrew Perry <perry.andrew.ml@gmail.com>
2023-02-08 23:10:22 +0000

    added helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Andrew P.
+# Date of lastest revision: 02/08/2023
+# Purpose: Print a string to the terminal
+
+
+# Main
+"I am working perfectly."
+
+# End
